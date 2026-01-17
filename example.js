d66e192398e67458d1db8a1523b0d59fcc0e498f
super-riya <riyasharmaofficial22@gmail.com>
2026-01-17 22:34:00 +0530

    Create example.js

---
+++ example.js
@@ -0,0 +1,57 @@
+export const firstNames = [
+  "Aarav",
+  "Vihaan",
+  "Aditya",
+  "Rohan",
+  "Kunal",
+  "Ananya",
+  "Priya",
+  "Sneha",
+  "Rahul",
+  "Neha",
+  "Arjun",
+  "Kavya",
+  "Anvith",
+  "Aaradhya",
+
+];
+
+export const lastNames = [
+  "Sharma",
+  "Verma",
+  "Singh",
+  "Gupta",
+  "Mehta",
+  "Patel",
+  "Iyer",
+  "Kulkarni",
+  "Malhotra",
+  "Chopra",
+  "Bansal",
+  "Reddy",
+  "Tiwari",
+  "Jha"
+];
+
+export const emails = [
+  "aarav.sharma@example.com",
+  "vihaan.verma@example.com",
+  "aditya.singh@example.com",
+  "rohan.gupta@example.com",
+  "kunal.mehta@example.com",
+  "ananya.iyer@example.com",
+  "priya.patel@example.com",
+  "sneha.kulkarni@example.com",
+  "rahul.malhotra@example.com",
+  "neha.chopra@example.com",
+  "arjun.bansal@example.com",
+  "kavya.reddy@example.com",
+  "anvith.tiwari@example.com",
+  "aaradhya.jha@example.com"
+];
+
+export const options = [
+    "No, I'm unable to comment.",
+    "Yes, I have commented my email ID.",
+    "Yes, I have commented my name.",
+];
