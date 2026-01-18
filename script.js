75a5da22a624d528c79c4c436ba82820dfd966ed
super-riya <riyasharmaofficial22@gmail.com>
2026-01-18 22:07:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,6 +70,6 @@ for (let run = 1; run <= MAX_RUNS; run++) {
 console.log("FINAL STATS:");
 console.log(JSON.stringify(stats, null, 2));
 // write stats ONCE
-fs.writeFileSync("stats.json", JSON.stringify(stats, null, 2));
+// fs.writeFileSync("stats.json", JSON.stringify(stats, null, 2));
 
 await browser.close();
